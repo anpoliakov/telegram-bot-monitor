bf6f79685c148f51726456ae6ebf0637485ad491
anpoliakov <Andrewlybimenko@gmail.com>
2020-10-12 22:09:20 +0300

    installation project

---
+++ src/main/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplication.java
@@ -0,0 +1,13 @@
+package by.andrew.bot.telegrambotmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.andrew.bot.telegrambotmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
