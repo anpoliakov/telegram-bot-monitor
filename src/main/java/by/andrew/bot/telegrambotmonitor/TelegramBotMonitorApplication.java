837a9a2bab243e938bc3c6ce8bc85b806455054e
anpoliakov <Andrewlybimenko@gmail.com>
2020-10-13 22:36:05 +0300

    naked project

--- src/main/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplication.java
+++ src/main/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplication.java
@@ -2,12 +2,12 @@ package by.andrew.bot.telegrambotmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TelegramBotMonitorApplication {
-
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(TelegramBotMonitorApplication.class, args);
     }
-
 }
