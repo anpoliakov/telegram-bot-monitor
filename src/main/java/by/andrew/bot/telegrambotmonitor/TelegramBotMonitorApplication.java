67c7b3418ff4610fa1d1f05134375bd70c4ceb58
anpoliakov <Andrewlybimenko@gmail.com>
2020-11-09 23:53:22 +0300

    telegram bot repeats your message

--- src/main/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplication.java
+++ src/main/java/by/andrew/bot/telegrambotmonitor/TelegramBotMonitorApplication.java
@@ -2,15 +2,11 @@ package by.andrew.bot.telegrambotmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TelegramBotMonitorApplication {
     public static void main(String[] args) {
-//        ApiContextInitializer.init();
+        // ApiContextInitializer.init();
         SpringApplication.run(TelegramBotMonitorApplication.class, args);
     }
-
-
 }
